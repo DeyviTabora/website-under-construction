4f063e76dd4b13e7f2828fdae2aff8b9f0382567
DeyviTabora <taborapaz@gmail.com>
2020-03-05 16:13:12 -0600

    Base website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
+import NavToggle from './components/NavToggle';
 import './App.css';
+import BodyMain from './components/BodyMain';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <NavToggle/>
+      <BodyMain/>
+      </div>
   );
 }
 
